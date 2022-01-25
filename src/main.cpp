3d5cf0f53d7fe08fb51a2dfccb550edc9be356dc
Hideki A. Ikeda <hidekiai@CodeMonkeyNinja.dev@users.noreply.github.com>
2022-01-25 14:28:16 -0600

    doing conditional compilation of lazy list vs memory array

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,9 @@
 
 int main()
 {
-    auto myMandelbrot = mandelbrot<double>(1000, -2.0, 1.0, -1.0, 1.0, 100, 24);
+    const auto width = 100;
+    const auto height = 24;
+    auto myMandelbrot = mandelbrot<double>(1000, -2.0, 1.0, -1.0, 1.0, width, height);
     auto myList = myMandelbrot.calculate_mandelbrot();
     myMandelbrot.render_mandelbrot(myList);
     return 0;
--- src/mandelbrot.hpp
+++ src/mandelbrot.hpp
@@ -1,21 +1,29 @@
 #pragma once
+
+// Use either list<T> (lazy) or vector<T> (memory)
+#define USE_ARRAY_INDEX
+
 #include <cstdlib>
-#include <list>
+#if defined(USE_ARRAY_INDEX)
 #include <vector>
+#else
+#include <list>
+#endif
 #include <complex>
-#include <algorithm>
+#include <algorithm> // std::for_each<>
+#include <iostream>  // cout
 
 template <class T>
 class mandelbrot
 {
 private:
-    size_t max_iters;
+    size_t max_iters;   // how deep we want the mandelbrot to be
     T x_min;
     T x_max;
     T y_min;
     T y_max;
-    size_t width;
-    size_t height;
+    size_t width;   // maybe rename this to screen_width
+    size_t height;  // screen_height?
 
 public:
     mandelbrot(
@@ -42,11 +50,12 @@ public:
 private:
     const size_t mandelbrot_at_point(const T &cx, const T &cy, const size_t &max_iters)
     {
-        auto z = std::complex<double>(0.0, 0.0);
-        const auto c = std::complex<double>(cx, cy); // immutable
+        auto z = std::complex<T>(0.0, 0.0);
+        const auto c = std::complex<T>(cx, cy); // immutable
+        const T minNorm = 2.0;
         for (auto i = 0; i <= max_iters; ++i)
         {
-            if (std::norm(z) > 2.0) // std::norm() is one of those that is static and it's not z.norm() member function...
+            if (std::norm(z) > minNorm) // std::norm() is one of those that is static and it's not z.norm() member function...
             {
                 return i;
             }
@@ -57,7 +66,11 @@ private:
     };
 
 public:
+#if defined(USE_ARRAY_INDEX)
+    const std::vector<std::vector<size_t>> calculate_mandelbrot()
+#else
     const std::list<std::list<size_t>> calculate_mandelbrot()
+#endif
     {
         // NOTE: The nested iteration probably do not need to look this complicated if lambdas
         // aren't used, but I wanted to (at one point) use std::for_each() with lambdas
@@ -65,68 +78,92 @@ public:
         // look more complicated than it should (compared to other functional language syntax)
         // and I felt I was bending over backwards, so I settled for the classic
         // for(for()) nested loop and be done with it...
-        auto all_rows = std::list<std::list<size_t>>(width);
-
+#if defined(USE_ARRAY_INDEX)
+        auto all_rows = std::vector<std::vector<size_t>>(height + 1);
+#else
+        std::list<std::list<size_t>> all_rows;            // we'll uses push_back() for now
+#endif
         const auto calcOuter =
             [](mandelbrot<T> &pThis, const size_t &img_y) // using '[&]' is an alternative to passing pThis, but I prefer this way
         {
             const auto calc =
                 [&](mandelbrot<T> &pThis, const size_t &img_x, const size_t &img_y) // using '[&]' is an alternative to passing pThis, but I prefer this way
             {
-                const auto cx = pThis.x_min + (pThis.x_max - pThis.x_min) * (img_x / pThis.width);
-                const auto cy = pThis.y_min + (pThis.y_max - pThis.y_min) * (img_y / pThis.height);
+                const T cx = pThis.x_min + (pThis.x_max - pThis.x_min) * (static_cast<T>(img_x) / static_cast<T>(pThis.width));
+                const T cy = pThis.y_min + (pThis.y_max - pThis.y_min) * (static_cast<T>(img_y) / static_cast<T>(pThis.height));
                 const auto escaped_at = pThis.mandelbrot_at_point(cx, cy, pThis.max_iters);
                 return escaped_at;
             };
-            auto row = std::list<size_t>(pThis.height);
-            //std::list<size_t> row = list(pThis.height);
+#if defined(USE_ARRAY_INDEX)
+            auto row = std::vector<size_t>(pThis.width + 1);
+#else
+            std::list<size_t> row;                        // using push_back() for now...
+#endif
             for (auto img_x = 0; img_x < pThis.width; ++img_x)
-                row.push_back(calc(pThis, img_x, img_y));
+#if defined(USE_ARRAY_INDEX)
+                row[img_x] = calc(pThis, img_x, img_y);
+#else
+                row.push_back(calc(pThis, img_x, img_y)); // cannot use push_back (but rather, swap/replace via index) if list is preallocated
+#endif
             return row;
         };
 
         for (auto img_y = 0; img_y < height; ++img_y)
         {
             const auto result = calcOuter(*this, img_y);
-            all_rows.push_back(result);
+#if defined(USE_ARRAY_INDEX)
+            all_rows[img_y] = result;
+#else
+            all_rows.push_back(result);                   // cannot use push_back (but rather, swap/replace via index) if list is preallocated
+#endif
         }
         return all_rows;
     };
 
+#if defined(USE_ARRAY_INDEX)
+    void render_mandelbrot(const std::vector<std::vector<size_t>> &escape_vals) const
+#else
     void render_mandelbrot(const std::list<std::list<size_t>> &escape_vals) const
+#endif
     {
         std::for_each(
             escape_vals.begin(),
             escape_vals.end(),
+#if defined(USE_ARRAY_INDEX)
+            [](const std::vector<size_t> &row)
+#else
             [](const std::list<size_t> &row)
+#endif
             {
-                auto line = std::string(row.size(), ' ');
+                // we'll use stringstream in hopes for better performance
+                std::stringstream line{};
                 std::for_each(
                     row.begin(),
                     row.end(),
-                    [](const size_t &column)
+                    [&](const size_t &column) // there are two ways, one is to let the lambda return char in which we'd stream via 'line<<result' or just pass line (via '[&]') and let it stream inside the lambda
                     {
                         // wish I had pattern matching in C++
-                        auto val = u8" ";
                         if (column <= 2)
-                            val = u8" ";
+                            line << " ";
                         else if (column > 2 && column <= 5)
-                            val = u8".";
+                            line << ".";
                         else if (column > 5 && column <= 10)
-                            val = u8"•";
+                            line << "•";
                         else if (column > 11 && column <= 30)
-                            val = u8"*";
+                            line << "*";
                         else if (column > 30 && column <= 100)
-                            val = u8"+";
+                            line << "+";
                         else if (column > 100 && column <= 200)
-                            val = u8"x";
+                            line << "x";
                         else if (column > 200 && column <= 400)
-                            val = u8"$";
+                            line << "$";
                         else if (column > 400 && column <= 700)
-                            val = u8"#";
+                            line << "#";
                         else
-                            val = u8"%";
+                            line << "%";
                     });
+                std::cout << line.str() << std::endl;
+                line.flush();
             });
     };
 };
