498926c171102a715840c71292d70c6a8847858e
Hideki A. Ikeda <hidekiai@CodeMonkeyNinja.dev@users.noreply.github.com>
2022-01-25 20:36:45 -0600

    Fixed minor bug of uninitialized variable in ctor - moved private method into (nested) lambdas - updated README

--- src/mandelbrot.hpp
+++ src/mandelbrot.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 // Use either list<T> (lazy) or vector<T> (memory)
-#define USE_ARRAY_INDEX
+//#define USE_ARRAY_INDEX
 
 #include <cstdlib>
 #if defined(USE_ARRAY_INDEX)
@@ -17,13 +17,13 @@ template <class T>
 class mandelbrot
 {
 private:
-    size_t max_iters;   // how deep we want the mandelbrot to be
+    size_t max_iters; // how deep we want the mandelbrot to be
     T x_min;
     T x_max;
     T y_min;
     T y_max;
-    size_t width;   // maybe rename this to screen_width
-    size_t height;  // screen_height?
+    size_t width;  // maybe rename this to screen_width
+    size_t height; // screen_height?
 
 public:
     mandelbrot(
@@ -36,6 +36,7 @@ public:
         const size_t &_height)
     {
         max_iters = _max_iters;
+        x_min = _x_min;
         x_max = _x_max;
         y_min = _y_min;
         y_max = _y_max;
@@ -46,24 +47,6 @@ public:
     ~mandelbrot()
     {
     }
-
-private:
-    const size_t mandelbrot_at_point(const T &cx, const T &cy, const size_t &max_iters)
-    {
-        auto z = std::complex<T>(0.0, 0.0);
-        const auto c = std::complex<T>(cx, cy); // immutable
-        const T minNorm = 2.0;
-        for (auto i = 0; i <= max_iters; ++i)
-        {
-            if (std::norm(z) > minNorm) // std::norm() is one of those that is static and it's not z.norm() member function...
-            {
-                return i;
-            }
-            z = z * z + c;
-        }
-        // couldn't find, so just return the max value
-        return max_iters;
-    };
 
 public:
 #if defined(USE_ARRAY_INDEX)
@@ -78,43 +61,64 @@ public:
         // look more complicated than it should (compared to other functional language syntax)
         // and I felt I was bending over backwards, so I settled for the classic
         // for(for()) nested loop and be done with it...
-#if defined(USE_ARRAY_INDEX)
-        auto all_rows = std::vector<std::vector<size_t>>(height + 1);
-#else
-        std::list<std::list<size_t>> all_rows;            // we'll uses push_back() for now
-#endif
-        const auto calcOuter =
-            [](mandelbrot<T> &pThis, const size_t &img_y) // using '[&]' is an alternative to passing pThis, but I prefer this way
+
+        const auto calc =
+            //[&mandelbrot_at_point]  // capture local lambda, or else you'll get "xyz is not captured"
+            [](mandelbrot<T> &pThis, const size_t &_img_x, const size_t &_img_y) // using '[&]' is an alternative to passing pThis, but I prefer this way
         {
-            const auto calc =
-                [&](mandelbrot<T> &pThis, const size_t &img_x, const size_t &img_y) // using '[&]' is an alternative to passing pThis, but I prefer this way
+            // Still new to C++ (well, coming back after many years) and don't know whether embedding lambdas inside lambdas causes instantiation
+            // performance issue or not; in other languages such as F#, I tend to localize multiple nested lambdas specific to its own purpose
+            // and am unsure if this is good idea (performance-wise) or not yet...
+            const auto mandelbrot_at_point =
+                [](const T &cx, const T &cy, const size_t &max_iters)
             {
-                const T cx = pThis.x_min + (pThis.x_max - pThis.x_min) * (static_cast<T>(img_x) / static_cast<T>(pThis.width));
-                const T cy = pThis.y_min + (pThis.y_max - pThis.y_min) * (static_cast<T>(img_y) / static_cast<T>(pThis.height));
-                const auto escaped_at = pThis.mandelbrot_at_point(cx, cy, pThis.max_iters);
-                return escaped_at;
+                std::complex<T> z = {0.0, 0.0};         // just in case we don't trust that default is (0, 0)
+                //const auto c = std::complex<T>(cx, cy); // immutable
+                const std::complex<T> c = {cx, cy}; // immutable
+                const T minNorm = 2.0;
+                for (size_t i = 0; i <= max_iters; ++i)
+                {
+                    // note, can probably use abs(z) instead of std::norm(z) for performance? (though compared to rust, C++ seems to be 2X faster)
+                    //if (std::norm(z) > minNorm) // std::norm() is one of those that is static and it's not z.norm() member function...
+                    if (abs(z) > minNorm) // using abs() instead of std::norm() results in small/slight diff (shifts image a bit to right)
+                    {
+                        return i;
+                    }
+                    z = z * z + c;
+                }
+                // couldn't find, so just return the max value
+                return max_iters;
             };
-#if defined(USE_ARRAY_INDEX)
-            auto row = std::vector<size_t>(pThis.width + 1);
-#else
-            std::list<size_t> row;                        // using push_back() for now...
-#endif
-            for (auto img_x = 0; img_x < pThis.width; ++img_x)
-#if defined(USE_ARRAY_INDEX)
-                row[img_x] = calc(pThis, img_x, img_y);
-#else
-                row.push_back(calc(pThis, img_x, img_y)); // cannot use push_back (but rather, swap/replace via index) if list is preallocated
-#endif
-            return row;
+
+            const T cx = pThis.x_min + (pThis.x_max - pThis.x_min) * (static_cast<T>(_img_x) / static_cast<T>(pThis.width));
+            const T cy = pThis.y_min + (pThis.y_max - pThis.y_min) * (static_cast<T>(_img_y) / static_cast<T>(pThis.height));
+            const auto escaped_at = mandelbrot_at_point(cx, cy, pThis.max_iters);
+            return escaped_at;
         };
 
+#if defined(USE_ARRAY_INDEX)
+        auto all_rows = std::vector<std::vector<size_t>>(height); // NOTE: on rust, this is initialized with width
+        auto row = std::vector<size_t>(width);
+#else
+        std::list<std::list<size_t>> all_rows;            // we'll uses push_back()
+        std::list<size_t> row;                            // using push_back()
+#endif
         for (auto img_y = 0; img_y < height; ++img_y)
         {
-            const auto result = calcOuter(*this, img_y);
+            //const auto result = calcOuter(*this, img_y);
+            for (auto img_x = 0; img_x < width; ++img_x)
+            {
 #if defined(USE_ARRAY_INDEX)
-            all_rows[img_y] = result;
+                row[img_x] = calc(*this, img_x, img_y);
 #else
-            all_rows.push_back(result);                   // cannot use push_back (but rather, swap/replace via index) if list is preallocated
+                row.push_back(calc(*this, img_x, img_y)); // cannot use push_back (but rather, swap/replace via index) if list is preallocated
+#endif
+            }
+#if defined(USE_ARRAY_INDEX)
+            all_rows[img_y] = row;
+#else
+            all_rows.push_back(row);                      // cannot use push_back (but rather, swap/replace via index) if list is preallocated
+            row.clear();                                  // have to clear (unlike with vector) or else it'll keep appending
 #endif
         }
         return all_rows;
